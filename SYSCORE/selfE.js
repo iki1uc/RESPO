6706d4e8f9518b477e8d9966bd53da38c5313527
iki1uc <entertrainer@web.de>
2026-07-11 20:12:38 +0200

    Update selfE.js

--- SYSCORE/selfE.js
+++ SYSCORE/selfE.js
@@ -1,1 +1,10 @@
+export function selfE(){
+    return {
+        E1: "Existenz = aktiv",
+        E2: "Ergründung = aktiv",
+        fusion: "Δ₁ + Δ₂ = □",
+        octa: "□ × 2 = OctaCore",
+        octa2: "OctaCore × Spiegel = OctaCore²"
+    };
+}
 
